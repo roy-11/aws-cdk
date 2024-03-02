4bcaf4c28dbf2da2537fbb2c53db59ef02105f11
roy-11 <63440731+roy-11@users.noreply.github.com>
2024-03-02 11:49:32 +0900

    chore: use env instead of context

--- bin/app.ts
+++ bin/app.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 // import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
+import { App, StackProps } from "aws-cdk-lib";
 import { EC2Stack } from "../lib/ec2-stack";
 
-const app = new cdk.App();
+const app = new App();
 
-const env = {
-  account: app.node.tryGetContext("account"),
-  region: app.node.tryGetContext("region"),
+const env: StackProps["env"] = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
 };
 
 new EC2Stack(app, "EC2Stack", {
